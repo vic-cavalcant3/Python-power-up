d262bfb6ca3a34f7ac45c7f4a9d90622c8a1df51
Victor Cavalcante <159542690+vic-cavalcant3@users.noreply.github.com>
2025-07-17 15:46:14 -0300

    Finish 17/07

--- sistema.py
+++ sistema.py
@@ -10,26 +10,26 @@ pyautogui.write("chrome")
 pyautogui.press("enter")
 
 #Entrando no site
+time.sleep(1)
 pyautogui.write("https://cadastrodalunos.netlify.app/")
 pyautogui.press("enter")
 time.sleep(2)
 
 # Fazendo o login no site
-pyautogui.click(x=1206 , y=589)
+pyautogui.press("tab")
 pyautogui.write("victorrocha0223@gmail.com")
 pyautogui.press("tab")
 pyautogui.write("12345")
 pyautogui.press("enter")
 time.sleep(0.2)
 pyautogui.press("enter")
-
 # Cadastrando os dados usando o pandas
 
 tabela = pandas.read_csv("./src/alunos_30.csv")
 print(tabela)
 
 for linha in tabela.index:
-    pyautogui.click(x=1038, y=311)
+    pyautogui.click(x=650, y=250)
 
     nome = tabela.loc[linha, "nome"]
     pyautogui.write(nome)
